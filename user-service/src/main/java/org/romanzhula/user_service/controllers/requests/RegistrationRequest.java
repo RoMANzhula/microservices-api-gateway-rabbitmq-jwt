703f56246143547aa614c65483a77a8a77b5b706
RoMANzhula <roman1985al@ukr.net>
2025-03-09 23:14:09 +0200

    Up RegistrationRequest - user-service

--- user-service/src/main/java/org/romanzhula/user_service/controllers/requests/RegistrationRequest.java
+++ user-service/src/main/java/org/romanzhula/user_service/controllers/requests/RegistrationRequest.java
@@ -14,5 +14,8 @@ public class RegistrationRequest {
     private String username;
     private String password;
     private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
 
 }
