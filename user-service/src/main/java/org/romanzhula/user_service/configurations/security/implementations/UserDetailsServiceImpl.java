2f4481422a3c9437e7122b194c3ff5f93eceb08e
RoMANzhula <roman1985al@ukr.net>
2025-03-09 23:21:04 +0200

    Add UserDetailsServiceImpl - user-service - security data

---
+++ user-service/src/main/java/org/romanzhula/user_service/configurations/security/implementations/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package org.romanzhula.user_service.configurations.security.implementations;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.models.User;
+import org.romanzhula.user_service.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username))
+        ;
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
