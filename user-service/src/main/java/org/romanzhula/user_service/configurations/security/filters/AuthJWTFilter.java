12cb431f54197c341b8cf8c52387b564d179bb71
RoMANzhula <roman1985al@ukr.net>
2025-03-11 18:05:30 +0200

    Add AuthJWTFilter - user-service - checking jwt-token in every http-request

---
+++ user-service/src/main/java/org/romanzhula/user_service/configurations/security/filters/AuthJWTFilter.java
@@ -0,0 +1,57 @@
+package org.romanzhula.user_service.configurations.security.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.configurations.security.implementations.UserDetailsServiceImpl;
+import org.romanzhula.user_service.configurations.security.jwt.JWTService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+public class AuthJWTFilter extends OncePerRequestFilter {
+
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String BEARER_KEYWORD_IN_HEADER = "Bearer ";
+
+    private final JWTService jwtService;
+    private final UserDetailsServiceImpl userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader(AUTH_HEADER);
+
+        if (authHeader == null || !authHeader.contains(BEARER_KEYWORD_IN_HEADER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwtToken = authHeader.substring(BEARER_KEYWORD_IN_HEADER.length());
+
+        String usernameFromToken = jwtService.extractUsernameFromToken(jwtToken);
+
+        if (usernameFromToken != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(usernameFromToken);
+
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
+
+            }
+        }
+    }
+
+}
