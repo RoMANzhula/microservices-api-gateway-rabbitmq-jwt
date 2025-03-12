81c10b3cfef7b0892277babc90aed46d626a9ee6
RoMANzhula <roman1985al@ukr.net>
2025-03-12 23:32:37 +0200

    Add CustomUserDetailsService - microservices-common - custom interface

---
+++ microservices-common/src/main/java/org/romanzhula/microservices_common/security/CustomUserDetailsService.java
@@ -0,0 +1,12 @@
+package org.romanzhula.microservices_common.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface CustomUserDetailsService extends UserDetailsService {
+
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+}
