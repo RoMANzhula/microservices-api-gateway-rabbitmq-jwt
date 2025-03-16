9a6c422279db178bbc2a1d59391a2f5724e4b873
RoMANzhula <roman1985al@ukr.net>
2025-03-16 23:41:28 +0200

    Up UserServiceApplication - user-service - add PageSerializationMode

--- user-service/src/main/java/org/romanzhula/user_service/UserServiceApplication.java
+++ user-service/src/main/java/org/romanzhula/user_service/UserServiceApplication.java
@@ -3,7 +3,10 @@ package org.romanzhula.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
