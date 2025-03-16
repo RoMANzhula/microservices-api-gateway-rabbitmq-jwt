3deb71ca680575ef99bbc05279ba2af07d91e630
RoMANzhula <roman1985al@ukr.net>
2025-03-16 23:25:01 +0200

    Up WebSecurityConfiguration - user-service

--- user-service/src/main/java/org/romanzhula/user_service/configurations/security/WebSecurityConfiguration.java
+++ user-service/src/main/java/org/romanzhula/user_service/configurations/security/WebSecurityConfiguration.java
@@ -3,16 +3,15 @@ package org.romanzhula.user_service.configurations.security;
 import lombok.RequiredArgsConstructor;
 import org.romanzhula.microservices_common.security.jwt.AuthEntryPointJwt;
 import org.romanzhula.microservices_common.security.jwt.AuthJWTFilter;
-import org.romanzhula.user_service.configurations.ApplicationConfiguration;
 import org.romanzhula.user_service.configurations.security.cors.CorsAutoConfiguration;
 import org.romanzhula.user_service.configurations.security.cors.EnableCORS;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,8 +26,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class WebSecurityConfiguration {
 
     private final AuthEntryPointJwt authEntryPointJwtUnauthorizedHandler;
-    private final ApplicationConfiguration applicationConfiguration;
     private final CorsAutoConfiguration corsAutoConfiguration;
+    private final AuthJWTFilter authJwtFilter;
+    private final DaoAuthenticationProvider daoAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -38,26 +38,33 @@ public class WebSecurityConfiguration {
                         .authenticationEntryPoint(authEntryPointJwtUnauthorizedHandler)
                 )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/img/**",
+                                "/css/**",
+                                "/js/**"
+                        ).permitAll()
                         .requestMatchers(
                                 "/swagger-ui.html",
-                                        "/swagger-ui/**",
-                                        "/swagger-resources/**",
-                                        "/swagger-resources",
-                                        "/v3/api-docs/**",
-                                        "/proxy/**"
-                                ).permitAll()
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/swagger-resources",
+                                "/v3/api-docs/**",
+                                "/proxy/**",
+                                "/webjars/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .httpBasic(withDefaults())
-                .authenticationProvider(applicationConfiguration.daoAuthenticationProvider())
-                .addFilterBefore(applicationConfiguration.authJWTFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(daoAuthenticationProvider)
+                .addFilterBefore(authJwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(corsAutoConfiguration.corsFilter(), AuthJWTFilter.class)
                 .headers((headers) ->
-                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                        headers
+                                .frameOptions((frameOptions) -> frameOptions.disable())
                 )
         ;
 
