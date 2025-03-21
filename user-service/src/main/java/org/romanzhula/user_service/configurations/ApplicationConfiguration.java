c7104d876ee8b0f64e32f39c943783e4c5c7df4e
RoMANzhula <roman1985al@ukr.net>
2025-03-21 21:39:01 +0200

    Up ApplicationConfiguration - user-service

--- user-service/src/main/java/org/romanzhula/user_service/configurations/ApplicationConfiguration.java
+++ user-service/src/main/java/org/romanzhula/user_service/configurations/ApplicationConfiguration.java
@@ -1,11 +1,14 @@
 package org.romanzhula.user_service.configurations;
 
 import lombok.RequiredArgsConstructor;
+import org.romanzhula.microservices_common.cors.CorsAutoConfiguration;
+import org.romanzhula.microservices_common.cors.CorsConfigurationProperties;
 import org.romanzhula.microservices_common.security.CustomUserDetailsService;
 import org.romanzhula.microservices_common.security.jwt.AuthEntryPointJwt;
 import org.romanzhula.microservices_common.security.jwt.AuthJWTFilter;
 import org.romanzhula.microservices_common.security.jwt.CommonJWTService;
 import org.romanzhula.user_service.configurations.security.implementations.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +22,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfiguration {
+
+    @Value("${app.jwt_secret_code}")
+    private String secretKey;
 
     private final UserDetailsServiceImpl userDetailServiceImpl;
 
@@ -68,7 +74,17 @@ public class ApplicationConfiguration {
 
     @Bean
     public CommonJWTService commonJWTService() {
-        return new CommonJWTService();
+        return new CommonJWTService(secretKey);
+    }
+
+    @Bean
+    public CorsAutoConfiguration corsAutoConfiguration() {
+        return new CorsAutoConfiguration(properties());
+    }
+
+    @Bean
+    public CorsConfigurationProperties properties() {
+        return new CorsConfigurationProperties();
     }
 
 }
